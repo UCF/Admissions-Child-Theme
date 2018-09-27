c156fb3063f9f7cb04f0c316be08080905356ea7
Jo Dickson <jogreybill@gmail.com>
2018-09-27 17:14:31 -0400

    Added stylesheet for colleges grid

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,18 @@ const browserSync  = require('browser-sync').create();
 const gulp         = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS     = require('gulp-clean-css');
-const include      = require('gulp-include');
-const eslint       = require('gulp-eslint');
-const isFixed      = require('gulp-eslint-if-fixed');
-const babel        = require('gulp-babel');
 const rename       = require('gulp-rename');
 const sass         = require('gulp-sass');
 const sassLint     = require('gulp-sass-lint');
-const uglify       = require('gulp-uglify');
 const merge        = require('merge');
 
 
 let config = {
   src: {
-    scssPath: './src/scss',
-    jsPath: './src/js'
+    scssPath: './src/scss'
   },
   dist: {
-    cssPath: './static/css',
-    jsPath: './static/js',
-    fontPath: './static/fonts'
+    cssPath: './static/css'
   },
   packagesPath: './node_modules',
   sync: false,
@@ -68,35 +60,6 @@ function buildCSS(src, dest) {
     .pipe(gulp.dest(dest));
 }
 
-// Base JS linting function (with eslint). Fixes problems in-place.
-function lintJS(src, dest) {
-  dest = dest || config.src.jsPath;
-
-  return gulp.src(src)
-    .pipe(eslint({
-      fix: true
-    }))
-    .pipe(eslint.format())
-    .pipe(isFixed(dest));
-}
-
-// Base JS compile function
-function buildJS(src, dest) {
-  dest = dest || config.dist.jsPath;
-
-  return gulp.src(src)
-    .pipe(include({
-      includePaths: [config.packagesPath, config.src.jsPath]
-    }))
-    .on('error', console.log) // eslint-disable-line no-console
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(rename({
-      extname: '.min.js'
-    }))
-    .pipe(gulp.dest(dest));
-}
-
 // BrowserSync reload function
 function serverReload(done) {
   if (config.sync) {
@@ -119,23 +82,6 @@ function serverServe(done) {
 
 
 //
-// Installation of components/dependencies
-//
-
-// Athena Framework web font processing
-gulp.task('move-components-athena-fonts', (done) => {
-  gulp.src([`${config.packagesPath}/ucf-athena-framework/dist/fonts/**/*`])
-    .pipe(gulp.dest(config.dist.fontPath));
-  done();
-});
-
-// Run all component-related tasks
-gulp.task('components', gulp.parallel(
-  'move-components-athena-fonts'
-));
-
-
-//
 // CSS
 //
 
@@ -154,31 +100,12 @@ gulp.task('css', gulp.series('scss-lint-theme', 'scss-build-theme'));
 
 
 //
-// JavaScript
-//
-
-// Run eslint on js files in src.jsPath
-gulp.task('es-lint-theme', () => {
-  return lintJS([`${config.src.jsPath}/*.js`], config.src.jsPath);
-});
-
-// Concat and uglify js files through babel
-gulp.task('js-build-theme', () => {
-  return buildJS(`${config.src.jsPath}/script.js`, config.dist.jsPath);
-});
-
-// All js-related tasks
-gulp.task('js', gulp.series('es-lint-theme', 'js-build-theme'));
-
-
-//
 // Rerun tasks when files change
 //
 gulp.task('watch', (done) => {
   serverServe(done);
 
   gulp.watch(`${config.src.scssPath}/**/*.scss`, gulp.series('css', serverReload));
-  gulp.watch(`${config.src.jsPath}/**/*.js`, gulp.series('js', serverReload));
   gulp.watch('./**/*.php', gulp.series(serverReload));
 });
 
@@ -186,4 +113,4 @@ gulp.task('watch', (done) => {
 //
 // Default task
 //
-gulp.task('default', gulp.series('components', 'css', 'js'));
+gulp.task('default', gulp.series('css'));
