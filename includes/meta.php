372aa100c35bb8688324f7b1adc7eb0f1e5a717a
Jo Dickson <jogreybill@gmail.com>
2018-09-28 08:57:22 -0400

    Moved stylesheet enqueue to includes/meta.php

---
+++ includes/meta.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Enqueue front-end css and js.
+ **/
+function admissions_enqueue_frontend_assets() {
+	// Register child theme stylesheet
+	$theme = wp_get_theme();
+	$theme_version = $theme->get( 'Version' );
+	wp_enqueue_style( 'style-child', ADMISSIONS_THEME_CSS_URL . '/style.min.css', array( 'style' ), $theme_version );
+}
+
+add_action( 'wp_enqueue_scripts', 'admissions_enqueue_frontend_assets', 11, 0 );
