5bc568226e4a6c52d34eef48951389d278558e0f
Jo Dickson <jogreybill@gmail.com>
2019-07-15 15:10:15 -0400

    Style adjustments; added onscroll active anchor watching and map marker tooltips

--- dev/custom-pages/campus-tour.js
+++ dev/custom-pages/campus-tour.js
@@ -1,28 +1,50 @@
 (function ($) {
 
   const anchorLinkSelector = '.js-anchor-link';
-  const scrollTime = 750;
+  const anchorSelector = '.js-anchor';
+  const mapMarkerSelector = '.map-marker';
+  const mapMarkerTextSelector = '.js-marker-tooltip-text';
+  const scrollTime = 750; // ms
+  const mapVisibilityMinimumWidth = 992; // px
 
 
-  function clearActiveAnchorLinks() {
-    $(anchorLinkSelector)
-      .filter('.active')
-      .removeClass('active');
+  // Returns any active anchor links.
+  function getActiveAnchorLinks() {
+    return $(anchorLinkSelector).filter('.active');
   }
 
+  // De-activates any active anchor links.
+  function clearActiveAnchorLinks() {
+    const $activeAnchorLinks = getActiveAnchorLinks();
+    if ($activeAnchorLinks.length) {
+      $activeAnchorLinks.removeClass('active');
+    }
+  }
+
+  // Returns the active anchor.
+  function getActiveAnchor() {
+    const $activeAnchorLinks = getActiveAnchorLinks();
+    if ($activeAnchorLinks.length) {
+      return $($activeAnchorLinks.first().attr('href'));
+    }
+    return null;
+  }
+
+  // Activates anchor links for the current visible anchor.
   function assignActiveAnchorLinks(hash) {
     $(anchorLinkSelector)
       .filter(`[href="${hash}"]`)
       .addClass('active');
   }
 
+  // Scrolls to the anchor specified.
   function scrollToAnchor(hash) {
     const $anchor = $(hash);
     // Autoscroll to section on page and update document url
     // if this is a valid page anchor.
     if ($anchor.length) {
       $('html, body').animate({
-        scrollTop: $anchor.offset().top
+        scrollTop: $anchor.offset().top + 1
       }, scrollTime);
 
       if (history.pushState) {
@@ -33,7 +55,40 @@
     }
   }
 
+  // Initializes inview watching for anchors on the page
+  function anchorScrollWatch() {
+    if ($(window).width() >= mapVisibilityMinimumWidth) {
+      const $anchors = $(anchorSelector);
+      const $visibleAnchors = $anchors.filter(function () {
+        return $(this).isInViewport();
+      });
 
+      if ($visibleAnchors.length) {
+        const $activeAnchor = $visibleAnchors.first();
+        if ($activeAnchor !== getActiveAnchor()) {
+          clearActiveAnchorLinks();
+          assignActiveAnchorLinks(`#${$activeAnchor.attr('id')}`);
+        }
+      } else {
+        clearActiveAnchorLinks();
+      }
+    }
+  }
+
+
+  // Returns whether or not a given element is visible in the viewport
+  $.fn.isInViewport = function () {
+    const elementTop = $(this).offset().top;
+    const elementBottom = elementTop + $(this).outerHeight();
+
+    const viewportTop = $(window).scrollTop();
+    const viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
+
+
+  // Initializes anchor links
   $.fn.anchorLink = function () {
 
     this.each((index, elem) => {
@@ -56,8 +111,16 @@
 
   };
 
+
   $(document).ready(() => {
     $(anchorLinkSelector).anchorLink();
+    $(mapMarkerSelector).tooltip({
+      title: function () {
+        return $(this).find(mapMarkerTextSelector).text();
+      }
+    });
   });
+
+  $(window).on('scroll resize', anchorScrollWatch);
 
 }(jQuery));
