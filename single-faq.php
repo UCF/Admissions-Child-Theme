93a9c7cf17f5f8fa083e7410d0e9295c580e2a60
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-06-08 13:16:06 -0400

    Updated to route users back to topic lists from FAQ single pages

--- single-faq.php
+++ single-faq.php
@@ -22,6 +22,11 @@ $related_faq_title = ( get_field( 'related-faq-title', $topic ) ) ? get_field( '
 $cta_text = get_field( 'faq-topic-footer-cta-text', $topic );
 $cta_url = site_url() . get_field( 'faq-topic-footer-cta-url', $topic );
 
+if ( $topic ) {
+	$cta_text = "View All $topic->name FAQs";
+	$cta_url  = get_term_link( $topic, 'topic' );
+}
+
 $tags = wp_get_post_tags( $post->ID, array( 'fields' => 'slugs' ) );
 $related_posts = UCF_FAQ_Common::get_related_faqs_by_tag( $tags, array( $post->ID ) );
 ?>
@@ -37,7 +42,7 @@ $related_posts = UCF_FAQ_Common::get_related_faqs_by_tag( $tags, array( $post->I
 			<?php endif; ?>
 			<?php if ( $cta_text && $cta_url ) : ?>
 				<div class="ucf-faq-footer">
-					<?php echo UCF_FAQ_Common::display_footer_cta( $cta_text, $cta_url ); ?>
+					<a href="<?php echo $cta_url; ?>" class="btn btn-outline-secondary mt-4"><?php echo $cta_text; ?></a>
 				</div>
 			<?php endif; ?>
 		</article>
