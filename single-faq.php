eaa55b85f748e2e76ab57fc6f018bc8f76fb0250
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-08-11 10:31:12 -0400

    Corrected syntax error

--- single-faq.php
+++ single-faq.php
@@ -59,7 +59,7 @@ $generate = UCF_FAQ_Config::get_option_or_default( 'add_json_data' );
 if ( $generate ) : ?>
 <script type="application/ld+json">
 <?php
-	if ( method_exists( UCF_FAQ_Common, 'generate_json_ld' ) ) {
+	if ( method_exists( 'UCF_FAQ_Common', 'generate_json_ld' ) ) {
 		echo UCF_FAQ_Common::generate_json_ld( array( $post ) );
 	}
 ?>
